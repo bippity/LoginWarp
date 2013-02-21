c7eb17529c8f9eb463e131e5870467e4fd17c705
Colin Bohn <colinbohn@gmail.com>
2013-02-20 23:37:21 -0800

    Init commit

---
+++ LoginWarp/LoginWarp/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace LoginWarpPlugin
+{
+    public class Config
+    {
+        public string Warp = "spawn";
+
+        public void Write(string path)
+        {
+            File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+        }
+
+        public static Config Read(string path)
+        {
+            if (!File.Exists(path))
+            {
+                return new Config();
+            }
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+        }
+    }
+}
---
+++ LoginWarp/LoginWarp/LoginWarp.cs
@@ -0,0 +1,58 @@
+﻿﻿using System;
+using System.IO;
+using Terraria;
+using TShockAPI;
+using TShockAPI.DB;
+using TShockAPI.Hooks;
+using Newtonsoft.Json;
+
+namespace LoginWarpPlugin
+{
+    [APIVersion(1, 12)]
+
+    public class LoginWarp : TerrariaPlugin
+    {
+        private Config Config = new Config();
+        public override Version Version
+        {
+            get { return new Version("1.0"); }
+        }
+        public override string Name
+        {
+            get { return "LoginWarp"; }
+        }
+        public override string Author
+        {
+            get { return "Colin"; }
+        }
+        public override string Description
+        {
+            get { return "Warps player to warp on login"; }
+        }
+
+        public LoginWarp(Main game)
+            : base(game)
+        {
+            Order = 10;
+        }
+        public override void Initialize()
+        {
+            PlayerHooks.PlayerLogin += onLogin;
+            Hooks.GameHooks.Initialize += OnInitialize;
+        }
+        void OnInitialize()
+		{
+			string path = Path.Combine(TShock.SavePath, "login-warp.json");
+			if (File.Exists(path))
+			{
+				Config = Config.Read(path);
+			}
+			Config.Write(path);
+		}
+        void onLogin(TShockAPI.Hooks.PlayerLoginEventArgs ply)
+        {
+           Warp warp = TShock.Warps.FindWarp(Config.Warp);
+           ply.Player.Teleport((int)warp.WarpPos.X, (int)warp.WarpPos.Y + 3);
+        }
+    }
+}
