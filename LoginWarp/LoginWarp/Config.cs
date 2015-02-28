9a3cb1a768af3ffe8e30744d27c54f04ac7b564f
Lucas Nicodemus <shank@shanked.me>
2015-02-28 10:00:19 -0700

    Update to TerrariaAPI 1.16

--- LoginWarp/LoginWarp/Config.cs
+++ LoginWarp/LoginWarp/Config.cs
@@ -3,29 +3,26 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-//using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace LoginWarpPlugin
 {
-    public class Config
-    {
-        public string Warp = "spawn";
+	public class Config
+	{
+		public string Warp = "spawn";
 
-        public void Write(string path)
-        {
-            //File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
-            File.WriteAllText("Name of warp:spawn");
-        }
+		public void Write(string path)
+		{
+			File.WriteAllText(path, JsonConvert.SerializeObject(this, Formatting.Indented));
+		}
 
-        public static Config Read(string path)
-        {
-            if (!File.Exists(path))
-            {
-                return new Config();
-            }
-            //return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
-            var text = File.ReadAllText(path);
-            return text.Split(':')[1].TrimStart(' ');
-        }
-    }
+		public static Config Read(string path)
+		{
+			if (!File.Exists(path))
+			{
+				return new Config();
+			}
+			return JsonConvert.DeserializeObject<Config>(File.ReadAllText(path));
+		}
+	}
 }
--- LoginWarp/LoginWarp/LoginWarp.cs
+++ LoginWarp/LoginWarp/LoginWarp.cs
@@ -4,43 +4,43 @@ using Terraria;
 using TShockAPI;
 using TShockAPI.DB;
 using TShockAPI.Hooks;
+using TerrariaApi;
+using TerrariaApi.Server;
 using Newtonsoft.Json;
 
 namespace LoginWarpPlugin
 {
-    [APIVersion(1, 12)]
+	[ApiVersion(1, 16)]
+	public class LoginWarp : TerrariaPlugin
+	{
+		private Config Config = new Config();
+		public override Version Version
+		{
+			get { return new Version("1.0"); }
+		}
+		public override string Name
+		{
+			get { return "LoginWarp"; }
+		}
+		public override string Author
+		{
+			get { return "nicatronTg, Colin, SnirkImmington"; }
+		}
+		public override string Description
+		{
+			get { return "Warps player to warp on login"; }
+		}
 
-    public class LoginWarp : TerrariaPlugin
-    {
-        private Config Config = new Config();
-        public override Version Version
-        {
-            get { return new Version("1.0"); }
-        }
-        public override string Name
-        {
-            get { return "LoginWarp"; }
-        }
-        public override string Author
-        {
-            get { return "Colin"; }
-        }
-        public override string Description
-        {
-            get { return "Warps player to warp on login"; }
-        }
-
-        public LoginWarp(Main game)
-            : base(game)
-        {
-            Order = 10;
-        }
-        public override void Initialize()
-        {
-            PlayerHooks.PlayerLogin += onLogin;
-            Hooks.GameHooks.Initialize += OnInitialize;
-        }
-        void OnInitialize()
+		public LoginWarp(Main game)
+			: base(game)
+		{
+		}
+		public override void Initialize()
+		{
+			PlayerHooks.PlayerPreLogin += onLogin;
+			TShock.Initialized += OnInitialize;
+		}
+		void OnInitialize()
 		{
 			string path = Path.Combine(TShock.SavePath, "login-warp.txt");
 			if (File.Exists(path))
@@ -49,11 +49,12 @@ namespace LoginWarpPlugin
 			}
 			Config.Write(path);
 		}
-        void onLogin(TShockAPI.Hooks.PlayerLoginEventArgs ply)
-        {
-           Warp warp = TShock.Warps.FindWarp(Config.Warp);
-           if (warp != null)
-           	ply.Player.Teleport((int)warp.WarpPos.X, (int)warp.WarpPos.Y + 3);
-        }
-    }
+		
+		void onLogin(PlayerPreLoginEventArgs ply)
+		{
+			Warp warp = TShock.Warps.Find(Config.Warp);
+			if (warp != null)
+				ply.Player.Teleport((int)warp.Position.X, (int)warp.Position.Y + 3);
+		}
+	}
 }
